8c010f93a806e69a0a90705bdfde7ccac591197f
johnregan3 <john.m.regan@gmail.com>
2013-08-23 04:00:03 -0500

    Adding Row Action

--- simple-copy-post.js
+++ simple-copy-post.js
@@ -1,26 +1,34 @@
-jQuery(document).ready( function() {
+jQuery( document ).ready( function() {
 
-	jQuery("#scpjr3-copy-post").click( function(event) {
+	jQuery( "#scpjr3-copy-post" ).click( function( event ) {
 		event.preventDefault;
-		post_id = jQuery(this).attr("data-post-id");
-		nonce = jQuery(this).attr("data-nonce");
+		post_id = jQuery( this ).attr( "data-post-id" );
+		nonce   = jQuery( this ).attr( "data-nonce" );
 
 		jQuery.ajax({
 			type : "post",
 			dataType : "json",
-			url : cpjr3Ajax.ajaxurl,
-			data : {action: "scpjr3_script", post_id : post_id, nonce: nonce},
-			success: function(response) {
-				if(response.type == "success") {
-					jQuery("#scpjr3-success-message").html(response.message)
-				} else if(response.type == "not-published") {
-					jQuery("#scpjr3-error-message").html(response.message);
+			url : scpjr3Ajax.ajaxurl,
+			data : { action : "scpjr3_script", post_id : post_id, nonce : nonce },
+			success: function( response ) {
+				if ( response.type == "success" ) {
+					jQuery( "#scpjr3-message" ).append( response.message );
+					jQuery( "#scpjr3-message" ).css( { 'backgroundColor' : '#ffffe0', 'border' : '1px solid #e6db55' } );
+				} else if ( response.type == "not-published" ) {
+					jQuery( "#scpjr3-message" ).append( response.message );
+					jQuery( "#scpjr3-message" ).css( { 'backgroundColor' : '#ffebe8', 'border' : '1px solid #c00' } );
 				} else {
-					jQuery("#scpjr3-error-message").html("Error copying Post.  Please save the Post and try again.");
+					jQuery( "#scpjr3-message" ).html( "Error copying Post.  Please save the Post and try again." );
+					jQuery( "#scpjr3-message" ).css( { 'backgroundColor' : '#ffebe8', 'border' : '1px solid #c00' } );
 				}
+				jQuery( "#scpjr3-message" ).fadeIn().delay('5000').fadeOut();;
+			},
+			error: function( jqXHR, textStatus, errorThrown ) {
+				console.log( jqXHR);
+				console.log( textStatus );
+				console.log( errorThrown );
 			}
 		});
-
 	});
 
 });
--- simple-copy-post.php
+++ simple-copy-post.php
@@ -26,6 +26,7 @@
  * @package Simple Copy Posts
  * @author John Regan
  * @version 1.0
+ * @todo Add Bulk Action support
  */
 
 /**
@@ -55,18 +56,13 @@ function scpjr3_scripts( ) {
 
 
 /**
- * Register Meta Box
+ * Register Submit Box Field
  *
  * @since 1.0
  */
 
 add_action( 'post_submitbox_misc_actions', 'scpjr3_content' );
 
-/**
- * Field Content
- *
- * @since 1.0
- */
 function scpjr3_content() {
 	global $post;
 
@@ -74,71 +70,82 @@ function scpjr3_content() {
 	$scpjr3_nonce = wp_create_nonce( 'scpjr3_nonce' );
 
 	// Render Content of the Meta Box
-	echo '</div><div class="misc-pub-section">'; //Yes, this is here on purpose.
-	echo '<div id="#scpjr3-success-message"></div>';
-	echo '<div id="#scpjr3-error-message"></div>';
-	echo '<p class="description">' . __( 'Create a duplicate of this Post or Page', 'scpjr3' ) . '</p>';
-	echo '<button id="scpjr3-copy-post" name="scpjr3_copy_post" value="scpjr3-copy-post" class="button-secondary" data-nonce="' . $scpjr3_nonce . '" data-post-id="' . $post->ID . '">' . __( 'Copy', 'scpjr3' ) . '</button>';
+	echo '</div><div class="misc-pub-section" style="text-align: right;">'; //Yes, this is here on purpose.
+	echo '<div id="scpjr3-message" style="display: none; margin-bottom: 5px; padding: 5px 10px; text-align: left;"></div>';
+
+	echo '<a href="#" id="scpjr3-copy-post" name="scpjr3_copy_post" value="scpjr3-copy-post" class="button-secondary" data-nonce="' . $scpjr3_nonce . '" data-post-id="' . $post->ID . '">' . __( 'Copy this Post', 'scpjr3' ) . '</a>';
 }
 
 
-add_action( 'wp_ajax_mcpjr3_script', 'scpjr3_action' );
+/**
+ * Copy Post Action
+ *
+ * Verifies nonce, checks for capabilities, then copies content from
+ * original post ($old_post) into a new post.  Then, it saves, and
+ * if it's a page, it copies the Page Template.  Finally, if this
+ * is an ajax request, a response is returned. If not, the page is refreshed.
+ *
+ * @since 1.0
+ */
+add_action( 'wp_ajax_scpjr3_script', 'scpjr3_action' );
 
 function scpjr3_action() {
 
-	$post_id = $_POST['post_id'];
-
-	if ( ! $post_id ) {
-		$response['type'] = 'not-published';
-		$response['message'] = 'Post not copied.  Source Post must be Published to be duplicated.';
-		echo json_encode( $response );
-		exit();
-	}
-
-
-	// Check Capabilities
-	if ( 'page' == $_REQUEST['post_type'] ) {
-		if ( ! current_user_can( 'edit_page', $post_id ) )
-			return;
+	if ( isset( $_GET['nonce'] ) && isset( $_GET['post_id'] ) ) {
+		$nonce = $_GET['nonce'];
+		$post_id = $_GET['post_id'];
 	} else {
-		if ( ! current_user_can( 'edit_post', $post_id ) )
-			return;
+		$nonce = $_POST['nonce'];
+		$post_id = $_POST['post_id'];
 	}
 
 	// Nonce Check
-	if ( ! isset( $_POST['scpjr3_nonce'] ) || ! wp_verify_nonce( $_POST['scpjr3_nonce'], plugin_basename( __FILE__ ) ) )
+	if ( ! isset( $nonce ) || ! wp_verify_nonce( $nonce, 'scpjr3_nonce' ) ) {
 		return;
+	}
 
+	// Check Capabilities
+	if ( ! current_user_can( 'edit_post', $post_id ) || ! current_user_can( 'edit_page', $post_id )) {
+		return;
+	}
 	// Get the Old Post
 	$old_post = get_post( $post_id );
+
+	//If Old Post is unpublished, exit.
+	if ( ( "publish" != $old_post->post_status ) && ( !empty($_SERVER['HTTP_X_REQUESTED_WITH'] ) && ( strtolower( $_SERVER['HTTP_X_REQUESTED_WITH'] ) == 'xmlhttprequest') ) ) {
+		$response['type'] = 'not-published';
+		$response['message'] = 'Copies can only be made of Published Posts.';
+		echo json_encode( $response );
+		die();
+	}
 
 	// Copy Post Object Values
 	$new_post = array(
 		'post_status'    => 'draft',
-		'menu_order'     => $old_post['menu_order'],
-		'post_type'      => $old_post['post_type'],
-		'comment_status' => $old_post['comment_status'],
-		'ping_status'    => $old_post['ping_status'],
-		'pinged'         => $old_post['pinged'],
-		'post_author'    => $old_post['post_author'],
-		'post_category'  => $old_post['post_category'],
-		'post_content'   => $old_post['post_content'],
-		'post_excerpt'   => $old_post['post_excerpt'],
-		'post_name'      => $old_post['post_name'],
-		'post_parent'    => $old_post['post_parent'],
-		'post_password'  => $old_post['post_password'],
-		'post_title'     => $old_post['post_title'],
-		'post_type'      => $old_post['post_type'],
-		'tags_input'     => $old_post['tags_input'],
-		'to_ping'        => $old_post['to_ping'],
-		'tax_input'      => $old_post['tax_input'],
+		'menu_order'     => $old_post->menu_order,
+		'post_type'      => $old_post->post_type,
+		'comment_status' => $old_post->comment_status,
+		'ping_status'    => $old_post->ping_status,
+		'pinged'         => $old_post->pinged,
+		'post_author'    => $old_post->post_author,
+		'post_category'  => $old_post->post_category,
+		'post_content'   => $old_post->post_content,
+		'post_excerpt'   => $old_post->post_excerpt,
+		'post_name'      => $old_post->post_name,
+		'post_parent'    => $old_post->post_parent,
+		'post_password'  => $old_post->post_password,
+		'post_title'     => $old_post->post_title . ' (copy)',
+		'post_type'      => $old_post->post_type,
+		'tags_input'     => $old_post->tags_input,
+		'to_ping'        => $old_post->to_ping,
+		'tax_input'      => $old_post->tax_input,
 	);
 
 	// Create new Post
 	$new_post_id = wp_insert_post( $new_post );
 
 	// Copy Page Template if necessary
-	if ( $new_post_id && ('page' == $old_post['post_type'] ) ) {
+	if ( $new_post_id && ('page' == $old_post->post_type ) ) {
 		$page_template = get_post_meta( $post_id, '_wp_page_template' );
 		update_post_meta( $new_post_id, '_wp_page_template', $page_template );
 	}
@@ -147,14 +154,38 @@ function scpjr3_action() {
 	if( $new_post_id ) {
 		$response['type'] = 'success';
 		$response['message'] = 'Post successfully copied.';
+	} else {
+		$response['type'] = 'error';
+		$response['message'] = 'There was an error copying this post.';
 	}
 
-	// Send Response
-	if ( $response ) {
-		echo json_encode( $response );
+	// If this is being processed with Ajax, return the response
+	if(!empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest') {
+		echo json_encode($response);
+		die();
+	// If not, redirect to the same page.
+	} else {
+		header("Location: ".$_SERVER["HTTP_REFERER"]);
 	}
-
-	exit();
 
 }
+
+
+/**
+ * Add Copy link to Table Row Actions
+ *
+ * @since 1.0
+ * @param  array  $actions  Default Row Actions
+ * @return array  $actions  Modified Row Actions
+ */
+
+add_filter('post_row_actions','scpjr3_row_actions', 10, 2);
+
+function scpjr3_row_actions( $actions, $post ) {
+	// Create Nonce
+	$scpjr3_nonce = wp_create_nonce( 'scpjr3_nonce' );
+	$actions['scjr3_copy'] = '<a href="' . admin_url( "admin-ajax.php?action=scpjr3_script&nonce=" . $scpjr3_nonce . "&post_id=" . $post->ID ) . '" >' . __( 'Copy', 'scpjr3' ) . '</a>';
+	return $actions;
+}
+
 ?>
